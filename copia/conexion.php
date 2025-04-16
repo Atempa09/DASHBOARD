da7443944e206aa1a8c4bda77c82a8b1bbd08425
FRANCISCO ATEMPA <156622118+Atempa09@users.noreply.github.com>
2025-04-16 14:56:22 -0600

    Delete copia directory

--- copia/conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	$mysqli = mysqli_connect("localhost", "root", "", "sistema");
-		if (!$mysqli) {
-    		die("ERROR: No se pudo conectar a la base de datos." . mysqli_connect_error());
-	}
-
-?>
